4b903bff07403d4357f4684a1bffc7f9777d32f3
quietdish <whale61@naver.com>
2026-06-29 15:51:13 +0900

    메인 수정

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router'
 
 
 import './index.css'
 import App from './App.jsx'
 
-// import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
